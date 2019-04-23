3a7b04fc58e50c52c0865b54c3e2533387f22c1e
Jason Wang <jasonw365151605@gmail.com>
2019-04-23 10:32:26 -0700

    Created armor and weapon classes, created getters and setters for pretty much everything

---
+++ FalloutMonsterHunter/Classes/ItemStack/ItemStack.vb
@@ -0,0 +1,29 @@
+Public Class ItemStack
+    Private itemType As Item
+    Private stackSize As Integer
+
+    Public Sub New(item As Item, size As Integer)
+        Me.itemType = item
+        If size > 0 Then
+            Me.stackSize = size
+        Else
+            Throw New System.Exception("Error: Decared stack size of 0")
+        End If
+    End Sub
+
+    Public Sub setItem(item As Item)
+        Me.itemType = item
+    End Sub
+
+    Public Function getItem(item As Item)
+        Return Me.itemType
+    End Function
+
+    Private Sub setSize(size As Integer)
+        Me.stackSize = size
+    End Sub
+
+    Private Function getSize()
+        Return Me.stackSize
+    End Function
+End Class
--- FalloutMonsterHunter/Classes/Items/Item.vb
+++ FalloutMonsterHunter/Classes/Items/Item.vb
@@ -2,11 +2,23 @@
     Private itemName As String = ""
     Private itemTier As Integer
 
-    Sub setItemName(name As String)
+    Public Sub New(name As String)
         Me.itemName = name
     End Sub
 
-    Function getItemName(name As String)
+    Public Sub setItemName(name As String)
+        Me.itemName = name
+    End Sub
+
+    Public Function getItemName()
         Return Me.itemName
+    End Function
+
+    Public Sub setItemTier(tier As Integer)
+        Me.itemTier = tier
+    End Sub
+
+    Public Function getItemTier()
+        Return Me.itemTier
     End Function
 End Class
