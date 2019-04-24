d294888fc22f5c068ccc2ae9eaa8ee1ce46db0c1
kevinlao.819 <kevinlao.819@gmail.com>
2019-04-23 21:47:28 -0400

    no

---
+++ FalloutMonsterHunter/Classes/Items/ItemArmor.vb
@@ -0,0 +1,26 @@
+﻿Public Class ItemArmor
+    Inherits Item
+
+    Private defense As Integer
+    Private bonusHealth As Integer
+
+    Public Sub New(name As String)
+        MyBase.New(name)
+    End Sub
+
+    Public Sub setDefense(defense As Integer)
+        Me.defense = defense
+    End Sub
+
+    Public Function getDefence()
+        Return Me.defense
+    End Function
+
+    Public Sub setBonusHealth(health As Integer)
+        Me.bonusHealth = health
+    End Sub
+
+    Public Function getBonusHealth()
+        Return Me.bonusHealth
+    End Function
+End Class
---
+++ FalloutMonsterHunter/Classes/Items/ItemWeapon.vb
@@ -0,0 +1,18 @@
+﻿Public Class ItemWeapon
+    Inherits Item
+
+    Private damage As Integer
+
+    Public Sub New(name As String, damage As Integer)
+        MyBase.New(name)
+        Me.damage = damage
+    End Sub
+
+    Public Sub setDamage(damage As Integer)
+        Me.damage = damage
+    End Sub
+
+    Public Function getDamage()
+        Return Me.damage
+    End Function
+End Class
